9f3ed4efcfa62cb78315f402649d32fdd79481cd
RCGAProds <rcgaproductions@gmail.com>
2024-09-21 20:11:48 +0200

    Qualification Styles Removed & Modal Centered

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var span = document.getElementsByClassName("close")[0];
 // Open the modal when the link is clicked
 link.onclick = function(event) {
     event.preventDefault(); // Prevent the default behavior of the link
-    modal.style.display = "block";
+    modal.style.display = "flex";
 }
 
 // Close the modal when the X is clicked
