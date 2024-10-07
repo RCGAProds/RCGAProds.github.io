9aabb0a2283706b2e003563a0028f47f10a7d67a
RCGAProds <rcgaproductions@gmail.com>
2024-10-07 05:11:04 +0200

    Added Portfolio Section & More
    Added flex_center style + Changed Certificates Animation into Sweet Animations (Reusable) + Moved window.onclick to CloseModals into PortfolioCloseModal Fuctions +

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,7 @@ span.onclick = function () {
 };
 
 // Close the modal when clicking outside the modal content
-window.onclick = function (event) {
-  if (event.target == modal) {
-    closeModal();
-  }
-};
+//Functions in the Portfolio JS
 
 // Function to close the modal
 function closeModal() {
@@ -80,24 +76,76 @@ textElements.forEach((textElement) => {
   });
 });
 
-//<!-- ===================== Certificates Animation ===================== -->
-document
-  .querySelectorAll('.certificates__item:not(.work-in-progress)')
-  .forEach((item) => {
-    item.addEventListener('mouseenter', () => {
+//<!-- ===================== Sweet Animations ===================== -->
+document.querySelectorAll('.sweet_anim').forEach((item) => {
+  item.addEventListener('mouseenter', () => {
+    if (item.classList.contains('certificates__item')) {
       item.style.animation = 'moveUpDown 1s ease-in-out infinite';
-    });
+    }
+  });
 
-    item.addEventListener('mouseleave', () => {
-      const computedStyle = getComputedStyle(item);
-      const transformValue = computedStyle.transform;
+  item.addEventListener('mouseleave', () => {
+    const computedStyle = getComputedStyle(item);
+    const transformValue = computedStyle.transform;
 
-      item.style.animation = 'none';
-      item.style.transform = transformValue;
+    item.style.animation = 'none';
+    item.style.transform = transformValue;
 
-      setTimeout(() => {
+    setTimeout(() => {
+      if (item.classList.contains('certificates__item')) {
         item.style.transition = 'transform 0.3s ease-in-out';
         item.style.transform = 'translateY(0)';
-      }, 10);
-    });
+      }
+    }, 10);
   });
+});
+
+//<!-- ===================== Modal Portfolio ===================== -->
+
+const portfolioModals = document.querySelectorAll('.portfolio__model');
+const imgCards = document.querySelectorAll('.img__card');
+const portfolioCloseBtns = document.querySelectorAll('.portfolio__close_btn');
+
+var portfolioModal = function (modalClick) {
+  portfolioModals[modalClick].classList.add('active');
+};
+
+imgCards.forEach((imgCard, i) => {
+  imgCard.addEventListener('click', () => {
+    portfolioModal(i);
+  });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+  portfolioCloseBtn.onclick = function () {
+    closePortModal();
+  };
+});
+
+window.onclick = function (event) {
+  // Close the modal when clicking outside the modal content
+  if (event.target == modal) {
+    closeModal();
+  }
+  // Close the portfolioModals when clicking outside the modal content
+  portfolioModals.forEach((portfolioModalView) => {
+    if (
+      event.target === portfolioModalView &&
+      portfolioModalView.classList.contains('active')
+    ) {
+      closePortModal();
+    }
+  });
+};
+
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closePortModal();
+  }
+});
+
+function closePortModal() {
+  portfolioModals.forEach((portfolioModalView) => {
+    portfolioModalView.classList.remove('active');
+  });
+}
