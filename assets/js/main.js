6d76b92eeeb2b89a673592d8984e04191a4ae068
RCGAProds <rcgaproductions@gmail.com>
2024-10-08 02:46:19 +0200

    Fixed Some Errors + Formated Files
    Adjusted no-select class + Adjusted containers with no-select class + Removed label from Clients and Added div.clients instead + Added label for="" into the contact__inputs + Added id, placeholder and autocomplete into the inputs + Fixed thing with the user-select order in CSS + More

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,128 +1,128 @@
 /* MENU SHOW & HIDDEN */
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close")
 
 /* MENU SHOW */
 /* Validate if exist */
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
-  });
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu")
+  })
 }
 
 /* MENU HIDDEN */
 /* Validate if exist */
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  });
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu")
+  })
 }
 
 /* REMOVE MENU MOBILE */
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll(".nav__link")
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu');
+  const navMenu = document.getElementById("nav-menu")
   // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu');
+  navMenu.classList.remove("show-menu")
 }
-navLink.forEach((n) => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener("click", linkAction))
 
 /* ===================== SKILLS ===================== */
-const skillsContent = document.getElementsByClassName('skills__content'),
-  skillsHeader = document.querySelectorAll('.skills__header');
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header")
 
 function toggleSkills() {
-  let itemClass = this.parentNode.className;
+  let itemClass = this.parentNode.className
 
   for (i = 0; i < skillsContent.length; i++) {
-    skillsContent[i].className = 'skills__content skills__close';
+    skillsContent[i].className = "skills__content skills__close"
   }
-  if (itemClass === 'skills__content skills__close') {
-    this.parentNode.className = 'skills__content skills__open';
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open"
   }
 }
 
-skillsHeader.forEach((el) => {
-  el.addEventListener('click', toggleSkills);
-});
+skillsHeader.forEach(el => {
+  el.addEventListener("click", toggleSkills)
+})
 
 /* ===================== QUALIFICATION TABS ===================== */
 /* ===================== TESTIMONIAL ===================== */
 /* ===================== SCROLL SECTIONS ACTIVE LINK ===================== */
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]")
 
 function scrollActive() {
-  const scrollY = window.scrollY;
+  const scrollY = window.scrollY
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id');
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionId = current.getAttribute("id")
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.add('active-link');
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link")
     } else {
       document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.remove('active-link');
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link")
     }
-  });
+  })
 }
-window.addEventListener('scroll', scrollActive);
+window.addEventListener("scroll", scrollActive)
 
 /* ===================== CHANGE SCROLL HEADER ===================== */
 function scrollHeader() {
-  const nav = document.getElementById('header');
+  const nav = document.getElementById("header")
   // When the scroll is greater than 0 viewport height, add the scroll-header class to the header tag
-  nav.classList.toggle('scroll-header', this.scrollY > 0);
+  nav.classList.toggle("scroll-header", this.scrollY > 0)
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader)
 
 /* ===================== SHOW SCROLL UP ===================== */
 function scrollUp() {
-  const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById("scroll-up")
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
-  else scrollUp.classList.remove('show-scroll');
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll")
+  else scrollUp.classList.remove("show-scroll")
 }
-window.addEventListener('scroll', scrollUp);
+window.addEventListener("scroll", scrollUp)
 
 /* ===================== DARK LIGHT THEME ===================== */
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'uil-sun';
+const themeButton = document.getElementById("theme-button")
+const darkTheme = "dark-theme"
+const iconTheme = "uil-sun"
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+const selectedTheme = localStorage.getItem("selected-theme")
+const selectedIcon = localStorage.getItem("selected-icon")
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+  document.body.classList.contains(darkTheme) ? "dark" : "light"
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun"
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
-    iconTheme
-  );
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme,
+  )
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme,
+  )
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+themeButton.addEventListener("click", () => {
   // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
   // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
-});
+  localStorage.setItem("selected-theme", getCurrentTheme())
+  localStorage.setItem("selected-icon", getCurrentIcon())
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,152 +1,152 @@
 //<!-- ===================== Modal System ===================== -->
 // Get DOM elements
-var modal = document.getElementById('myModal');
-var links = document.querySelectorAll('.openModalLink');
-var span = document.getElementsByClassName('close')[0];
+var modal = document.getElementById("myModal")
+var links = document.querySelectorAll(".openModalLink")
+var span = document.getElementsByClassName("close")[0]
 
 // Open the modal when any link is clicked
 links.forEach(function (link) {
   link.onclick = function (event) {
-    event.preventDefault(); // Prevent the default behavior of the link
-    modal.style.display = 'flex';
+    event.preventDefault() // Prevent the default behavior of the link
+    modal.style.display = "flex"
 
     // Forzar el reflow para reiniciar la animación
-    void modal.querySelector('.modal-content').offsetWidth;
+    void modal.querySelector(".modal-content").offsetWidth
 
-    modal.querySelector('.modal-content').classList.add('show');
-  };
-});
+    modal.querySelector(".modal-content").classList.add("show")
+  }
+})
 
 // Close the modal when the X is clicked
 span.onclick = function () {
-  closeModal();
-};
+  closeModal()
+}
 
 // Close the modal when clicking outside the modal content
 //Functions in the Portfolio JS
 
 // Function to close the modal
 function closeModal() {
-  var content = modal.querySelector('.modal-content');
-  content.classList.remove('show');
+  var content = modal.querySelector(".modal-content")
+  content.classList.remove("show")
 
   setTimeout(() => {
-    modal.style.display = 'none';
-  }, 300); // The time must match the duration of the animation.
+    modal.style.display = "none"
+  }, 300) // The time must match the duration of the animation.
 }
 
 //<!-- ===================== CopyText ===================== -->
 // Get the paragraph element
-const textElements = document.querySelectorAll('.textToCopy');
+const textElements = document.querySelectorAll(".textToCopy")
 
-textElements.forEach((textElement) => {
+textElements.forEach(textElement => {
   const messageElement = document.getElementById(
-    'copyText' + textElement.id.replace('copy', '')
-  );
+    "copyText" + textElement.id.replace("copy", ""),
+  )
 
-  let hideTimeout;
+  let hideTimeout
 
   // Add event listener for clicks
-  textElement.addEventListener('click', function () {
+  textElement.addEventListener("click", function () {
     // Get the text content of the <p> element
-    const textToCopy = textElement.textContent;
+    const textToCopy = textElement.textContent
 
     // Copy the text to the clipboard using the Clipboard API
     navigator.clipboard
       .writeText(textToCopy)
       .then(function () {
         // Show confirmation message
-        messageElement.style.display = 'inline-block';
+        messageElement.style.display = "inline-block"
 
-        messageElement.classList.remove('scale-up');
+        messageElement.classList.remove("scale-up")
 
-        void messageElement.offsetWidth;
+        void messageElement.offsetWidth
 
-        messageElement.classList.add('scale-up');
+        messageElement.classList.add("scale-up")
 
-        clearTimeout(hideTimeout);
+        clearTimeout(hideTimeout)
 
         hideTimeout = setTimeout(() => {
-          messageElement.style.display = 'none';
-        }, 2500);
+          messageElement.style.display = "none"
+        }, 2500)
       })
       .catch(function (err) {
-        console.error('Error copying text: ', err);
-      });
-  });
-});
+        console.error("Error copying text: ", err)
+      })
+  })
+})
 
 //<!-- ===================== Sweet Animations ===================== -->
-document.querySelectorAll('.sweet_anim').forEach((item) => {
-  item.addEventListener('mouseenter', () => {
-    if (item.classList.contains('certificates__item')) {
-      item.style.animation = 'moveUpDown 1s ease-in-out infinite';
+document.querySelectorAll(".sweet_anim").forEach(item => {
+  item.addEventListener("mouseenter", () => {
+    if (item.classList.contains("certificates__item")) {
+      item.style.animation = "moveUpDown 1s ease-in-out infinite"
     }
-  });
+  })
 
-  item.addEventListener('mouseleave', () => {
-    const computedStyle = getComputedStyle(item);
-    const transformValue = computedStyle.transform;
+  item.addEventListener("mouseleave", () => {
+    const computedStyle = getComputedStyle(item)
+    const transformValue = computedStyle.transform
 
-    item.style.animation = 'none';
-    item.style.transform = transformValue;
+    item.style.animation = "none"
+    item.style.transform = transformValue
 
     setTimeout(() => {
-      if (item.classList.contains('certificates__item')) {
-        item.style.transition = 'transform 0.3s ease-in-out';
-        item.style.transform = 'translateY(0)';
+      if (item.classList.contains("certificates__item")) {
+        item.style.transition = "transform 0.3s ease-in-out"
+        item.style.transform = "translateY(0)"
       }
-    }, 10);
-  });
-});
+    }, 10)
+  })
+})
 
 //<!-- ===================== Modal Portfolio ===================== -->
 
-const portfolioModals = document.querySelectorAll('.portfolio__model');
-const imgCards = document.querySelectorAll('.img__card');
-const portfolioCloseBtns = document.querySelectorAll('.portfolio__close_btn');
+const portfolioModals = document.querySelectorAll(".portfolio__model")
+const imgCards = document.querySelectorAll(".img__card")
+const portfolioCloseBtns = document.querySelectorAll(".portfolio__close_btn")
 
 var portfolioModal = function (modalClick) {
-  portfolioModals[modalClick].classList.add('active');
-};
+  portfolioModals[modalClick].classList.add("active")
+}
 
 imgCards.forEach((imgCard, i) => {
-  imgCard.addEventListener('click', () => {
-    portfolioModal(i);
-  });
-});
+  imgCard.addEventListener("click", () => {
+    portfolioModal(i)
+  })
+})
 
-portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+portfolioCloseBtns.forEach(portfolioCloseBtn => {
   portfolioCloseBtn.onclick = function () {
-    closePortModal();
-  };
-});
+    closePortModal()
+  }
+})
 
 window.onclick = function (event) {
   // Close the modal when clicking outside the modal content
   if (event.target == modal) {
-    closeModal();
+    closeModal()
   }
   // Close the portfolioModals when clicking outside the modal content
-  portfolioModals.forEach((portfolioModalView) => {
+  portfolioModals.forEach(portfolioModalView => {
     if (
       event.target === portfolioModalView &&
-      portfolioModalView.classList.contains('active')
+      portfolioModalView.classList.contains("active")
     ) {
-      closePortModal();
+      closePortModal()
     }
-  });
-};
+  })
+}
 
-window.addEventListener('keydown', function (event) {
-  if (event.key === 'Escape') {
-    closePortModal();
-    closeModal();
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closePortModal()
+    closeModal()
   }
-});
+})
 
 function closePortModal() {
-  portfolioModals.forEach((portfolioModalView) => {
-    portfolioModalView.classList.remove('active');
-  });
+  portfolioModals.forEach(portfolioModalView => {
+    portfolioModalView.classList.remove("active")
+  })
 }
